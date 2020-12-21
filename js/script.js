06cd831f467f6dc4489371b7ab9813a837c2ec1c
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-21 17:08:42 +0100

    exercise  completed

--- js/script.js
+++ js/script.js
@@ -5,20 +5,57 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+//Inserire se pari o dispari
 var scelta = prompt('Scegli tra pari o dispari');
+var sceltaMin = scelta.slice(0).toLowerCase();
+
+if (sceltaMin != 'pari' && sceltaMin != 'dispari'){
+  alert('Hai inserito una scelta sbagliata, \nricarica la pagina!')
+}
+
+//inserire un numero da 1 a 5
 var numero = parseInt(prompt('Scegli un numero da 1 a 5'))
 
-console.log(scelta);
+ if (numero > 5 || numero < 1) {
+  alert('Hai inserito un numero sbagliato, \nricarica la pagina!')
+}
+
+console.log(sceltaMin);
 console.log(numero);
 
+//numero generato casulamente da 1 a 5
 var pcNumber = Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
 console.log(pcNumber);
 
+//somma dei numeri
 var somma = numero + pcNumber
 console.log(somma);
 
-if (somma % 2 == 0 && scelta == 'pari') {
+//senza funzione
+// if (somma % 2 == 0 && sceltaMin == 'pari') {
+//   console.log('Hai vinto');
+// }else {
+//   console.log('hai perso');
+// }
+
+// risultatose pari o dispari
+var probabilità = sommaNumeri(somma)
+console.log(probabilità);
+
+//vittoria o perdita
+if (sceltaMin == probabilità) {
   console.log('Hai vinto');
 }else {
-  console.log('hai perso');
+  console.log('Hai perso');
+}
+
+//funzione che determina se pari o dispari
+function sommaNumeri(sommaDeiNumeri) {
+  var risultato = 'dispari';
+
+  if (somma % 2 == 0 ) {
+    risultato = 'pari'
+    return risultato;
+  }
+  return risultato;
 }
